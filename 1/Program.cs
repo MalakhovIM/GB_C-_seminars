6b1bc9a99c44d1cd75ad99452074ce690d181947
IgorMalakhov <forvord_next@mail.ru>
2022-11-29 11:58:54 +0300

    add 2 programm

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿// Задача 1
+
+// Console.Write("Input a number: a ");
+
+// int usernumA = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Input a number: b ");
+
+// int usernumB = Convert.ToInt32(Console.ReadLine());
+
+// int quad = usernumB * usernumB;
+
+// if (usernumA == quad)
+// {
+//     Console.WriteLine("Yes!");
+// }
+// else
+// {
+//     Console.WriteLine("No!");
+// }
+
+//Задача 2
+
+// Console.Write("Input n ");
+
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int current = -n;
+
+// while(current <= n)
+// {
+//     Console.Write(current + " ");
+//     current++;
+
+// }
+
+// Задача 3
+
